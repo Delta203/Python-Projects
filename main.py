0d79b7bcb4409671b3f32f6e43f66acb804eb9dd
Delta203 <ddelta203@gmail.com>
2021-12-02 12:53:28 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+def monkey(word : str):
+  letters = "abcdefghijklmnopqrstuvwxyz"
+  plain = ""
+  prints = 0
+  
+  while plain[-len(word):] != word:
+    rdm = random.randint(0,len(letters)-1)
+    target = letters[rdm]
+    plain += target
+    prints += 1
+
+  print(plain, prints)
+
+monkey("oooo")
