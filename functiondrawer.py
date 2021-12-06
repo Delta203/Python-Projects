933c8fe9ffaaad4efd6d735926f36805149bfd0b
Delta203 <ddelta203@gmail.com>
2021-12-06 18:04:47 +0100

    Update functiondrawer.py

--- functiondrawer.py
+++ functiondrawer.py
@@ -30,24 +30,64 @@ class CoordinateSystem:
         line += j
       print(line)
 
-  def writeByXandY(self, x: float, y: int, val: str = "X"):
+  def writeByXandY(self, x: float, y: int):
+    val = "X"
+    if int(str(x).split(".")[1]) >= 3 and int(str(x).split(".")[1]) <= 7: val = "x"
+  
     x = x*2
     y = y*(-1)
     y = y*2 # decimal optimisation
+    
     try:
       self.coordinateSystem[int(y/2)+1+int(self.size)][int(x)+self.size*2] = val # y+1 cause Ʌ y line
     except: pass
   
-  def function(self, val : str):
+  def highAndLows(self, func: str, step: int = 0.001) -> list:
+    n = -float(self.size)
+    result = []
+    state = ""
+    while n <= self.size:
+      res = eval(func.replace("x", "("+str(n)+")"))
+      res_next = eval(func.replace("x", "("+str(n + step)+")"))
+      if res > res_next and state != "DOWN": 
+        state = "DOWN"
+        result.append(("PEAK",float("{:.5f}".format(n)),float("{:.5f}".format(res))))
+      elif res < res_next and state != "UP":
+        state = "UP"
+        result.append(("NOUN",float("{:.5f}".format(n)),float("{:.5f}".format(res))))
+      n += step
+    return result[1:]
+    
+  def zeroPoints(self, func: str, step: int = 0.001) -> list:
+    n = -float(self.size)
+    result = []
+    search = True
+    target = eval(func.replace("x", "("+str(n)+")")) # first index
+    while n <= self.size:
+      target_next = eval(func.replace("x", "("+str(n + step)+")"))
+      if abs(target-0) >= abs(target_next-0):
+        target = target_next
+        search = True
+      else:
+        if search == True: 
+          result.append((float("{:.5f}".format(n)),float("{:.5f}".format(target))))
+          search = False
+        target = eval(func.replace("x", "("+str(n)+")")) # set new index if something was found
+      
+      #print(n, target, search) Debug
+      n += step
+        
+    return result
+  
+  def function(self, func: str):
     for i in range(-self.size*2, self.size*2+1):
       i = i/2
       
       try:
-        equation = eval(val.replace("x", "("+str(i)+")"))
+        equation = eval(func.replace("x", "("+str(i)+")"))
         equation = float("{:.1f}".format(equation))
         if equation <= self.size: 
-          if equation % 1 == 0.0: self.writeByXandY(i, equation)
-          else: self.writeByXandY(i, equation, "x")
+          self.writeByXandY(i, equation)
       except: pass
 
 def rawFunction(val: str) -> str:
@@ -84,3 +124,6 @@ cs.function(func)
 cs.printCoordinateSystem()
 
 print("-> Function: " + func)
+print("Please wait for more information..")
+print("Peaks & Nouns: ", cs.highAndLows(func))
+print("Zeropoints: ", cs.zeroPoints(func))
